d3c2dbdb7d922865faf14186e0ae18c73f960f87
anand4756 <anand.k4756@gmail.com>
2023-08-30 11:24:06 +0530

    added python files

---
+++ server.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express')
+const cors = require('cors')
+const bodyparser = require('body-parser')
+
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+
+
+app.get('/python', (req,res)=>{
+    var spawn = require("child_process").spawn;
+      var process = spawn('python3',["./main.py"] );
+  
+
+
+    process.stdout.on('data', function(data) {
+        res.send(data.toString());
+    } )
+})
+
+
+
+
+
+
+
+const port = 5000 || process.env.PORT;
+
+app.listen(port,()=>{
+    console.log(`server started at ${port}`);
+})
