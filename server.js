4adddbc3433ba4ac12927ff7a224558493b650c1
Raj-101 <procrast95@gmail.com>
2023-08-31 09:57:13 +0530

    Created generalized utility functions to check and activate virtual environements. Added Python deps to '.gitignore' file.

--- server.js
+++ server.js
@@ -1,85 +1,84 @@
-require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
-const bodyparser = require("body-parser");
-
+const { spawn } = require("child_process");
 
 const app = express();
-app.use(cors());
+const port = process.env.PORT || 5000;
+
 app.use(express.json());
 
+const runPythonScript = (scriptPath, callback) => {
+    const virtualEnvPath = 'TEST_ENV';
+    const activationCommand = process.platform === 'win32' ? `.\\${virtualEnvPath}\\Scripts\\activate &&` : `source ./${virtualEnvPath}/bin/activate &&`;
+
+    const pythonScriptCommand = `python ${scriptPath}`;
+
+    const fullCommand = `${activationCommand} ${pythonScriptCommand}`;
+
+	console.log('FULL COMMAND:', fullCommand);
+
+    const pyProcess = spawn(fullCommand, { shell: true });
+
+    let output = '';
+    let error = '';
+
+    pyProcess.stdout.on("data", (data) => {
+        output += data.toString();
+    });
+
+    pyProcess.stderr.on("data", (data) => {
+        error += data.toString();
+    });
+
+    pyProcess.on("close", (code) => {
+        if (code === 0) {
+            callback(null, output);
+        } else {
+            callback(error, null);
+        }
+    });
+}
 
 app.get("/", (req, res) => {
-	const msg = `HeLLo from Server side 👋...!!!`;
-	res.send(msg);
-})
+    const msg = `Hello from the server side!`;
+    res.send(msg);
+});
 
 app.get("/python-print", (req, res) => {
-  try {
-	var spawn = require("child_process").spawn;
-	var pyProcess = spawn("python", ["./printMessage.py"]);
-
-	pyProcess.stdout.on("data", (data) => {
-		const dataStr = data.toString();
-		console.log(dataStr);
-		res.send(dataStr);
-	});
-
-	pyProcess.stderr.on("data", (err) => {
-		const e = err.toString();
-		console.error('Error:', e);
-	});
-  } catch (err) {
-	console.log(err);
-	res.send(err);
-  }
+    runPythonScript("./python-scripts/printMessage.py", (error, output) => {
+        if (error) {
+            console.error('Error:', error);
+            res.status(500).send(error);
+        } else {
+            console.log(output);
+            res.send(output);
+        }
+    });
 });
 
 app.get("/python", (req, res) => {
-  try {
-	var spawn = require("child_process").spawn;
-	var pyProcess = spawn("python", ["./main.py"]);
-
-	pyProcess.stdout.on("data", (data) => {
-		const dataStr = data.toString();
-		console.log(dataStr);
-		res.send(dataStr);
-	});
-
-	pyProcess.stderr.on("data", (err) => {
-		const e = err.toString();
-		console.error('Error:', e);
-	});
-  } catch (err) {
-	console.log(err);
-	res.send(err);
-  }
+    runPythonScript("./python-scripts/main.py", (error, output) => {
+        if (error) {
+            console.error('Error:', error);
+            res.status(500).send(error);
+        } else {
+            console.log(output);
+            res.send(output);
+        }
+    });
 });
 
 app.get("/python-numpy", (req, res) => {
-  try {
-	var spawn = require("child_process").spawn;
-	var pyProcess = spawn("python", ["./printNumpy.py"]);
-
-	pyProcess.stdout.on("data", (data) => {
-		const dataStr = data.toString();
-		console.log(dataStr);
-		res.send(dataStr);
-	});
-
-	pyProcess.stderr.on("data", (err) => {
-		const e = err.toString();
-		console.error('Error:', e);
-	});
-  } catch (err) {
-	console.log(err);
-	res.send(err);
-  }
+    runPythonScript("./python-scripts/printNumpy.py", (error, output) => {
+        if (error) {
+            console.error('Error:', error);
+            res.status(500).send(error);
+        } else {
+            console.log(output);
+            res.send(output);
+        }
+    });
 });
 
-
-const port = process.env.PORT || 5000;
-
 app.listen(port, () => {
-  console.log(`Server running at port '${port}'...`);
+    console.log(`Server running at port ${port}...`);
 });
